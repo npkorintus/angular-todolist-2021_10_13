432ead57d98620cd7624b2e8e10db61da344eecc
npkorintus <nicholas.korintus@gmail.com>
2021-10-13 18:48:59 -0400

    3 files modified, 1 added

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,12 +4,24 @@ import { FormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
-  MatCardModule, MatListModule, MatButtonModule, MatInputModule, MatExpansionModule, MatToolbarModule, MatIconModule, MatSnackBarModule, MatSlideToggleModule, MatChipsModule
+  MatCardModule,
+  MatListModule,
+  MatButtonModule,
+  MatInputModule,
+  MatExpansionModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSnackBarModule,
+  MatSlideToggleModule,
+  MatChipsModule,
+  MatDialogModule,
+  MatSelectModule,
 } from '@angular/material';
-import { AngularFireModule, } from 'angularfire2';
+import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AppComponent } from './app.component';
-import { TodoComponent } from './todo.component';
+import { TodoComponent, TodoModal } from './todo.component';
+// import { SelectComponent } from './select/select.component';
 
 @NgModule({
   imports: [
@@ -26,17 +38,20 @@ import { TodoComponent } from './todo.component';
     MatSnackBarModule,
     MatSlideToggleModule,
     MatChipsModule,
+    MatDialogModule,
+    MatSelectModule,
     AngularFirestoreModule,
     AngularFireModule.initializeApp({
-      apiKey: "AIzaSyCysz8O0EuHgF8VNQMaC3aXRWb7uGAwHVI",
-      authDomain: "todolistapp-stackblitz.firebaseapp.com",
-      databaseURL: "https://todolistapp-stackblitz.firebaseio.com",
-      projectId: "todolistapp-stackblitz",
-      storageBucket: "todolistapp-stackblitz.appspot.com",
-      messagingSenderId: "907624983083"
+      apiKey: 'AIzaSyCysz8O0EuHgF8VNQMaC3aXRWb7uGAwHVI',
+      authDomain: 'todolistapp-stackblitz.firebaseapp.com',
+      databaseURL: 'https://todolistapp-stackblitz.firebaseio.com',
+      projectId: 'todolistapp-stackblitz',
+      storageBucket: 'todolistapp-stackblitz.appspot.com',
+      messagingSenderId: '907624983083',
     }),
   ],
-  declarations: [AppComponent, TodoComponent],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, TodoComponent, TodoModal],
+  bootstrap: [AppComponent],
+  entryComponents: [TodoComponent, TodoModal],
 })
-export class AppModule { }
+export class AppModule {}
--- app/todo.component.ts
+++ app/todo.component.ts
@@ -1,93 +1,108 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import {
   AngularFirestore,
   AngularFirestoreCollection,
-  AngularFirestoreDocument
+  AngularFirestoreDocument,
 } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { MatSnackBar } from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Priority } from './select/select.component';
 
-interface Todo {
+export interface Todo {
   content: string;
   id?: string;
   datemodified?: Date;
   isDone?: boolean;
+  // selectedPriority: string;
 }
 @Component({
   selector: 'todo',
   templateUrl: './todo.component.html',
 })
-
 export class TodoComponent implements OnInit {
   todoCollection: AngularFirestoreCollection<Todo>;
   todoList: Observable<Todo[]>;
   todoDoc: AngularFirestoreDocument<Todo>;
   inputId: string;
   inputValue: Todo = {
-    content: "",
-  }
+    content: '',
+    // selectedPriority: '',
+    // priority: {
+    //   value: '',
+    //   viewValue: '',
+    // },
+  };
 
   editValue: boolean = false;
-  constructor(public afs: AngularFirestore, public snackBar: MatSnackBar) {
-  }
+  constructor(
+    public afs: AngularFirestore,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog
+  ) {}
   ngOnInit() {
-
     this.todoCollection = this.afs.collection('Todolist');
-    this.todoList = this.afs.collection('Todolist', ref => ref.orderBy('datemodified')).snapshotChanges().map(changes => {
-      return changes.map(a => {
-        const data = a.payload.doc.data() as Todo;
-        data.id = a.payload.doc.id;
-        return data;
-      })
-    })
+    this.todoList = this.afs
+      .collection('Todolist', (ref) => ref.orderBy('priority'))
+      .snapshotChanges()
+      .map((changes) => {
+        return changes.map((a) => {
+          const data = a.payload.doc.data() as Todo;
+          data.id = a.payload.doc.id;
+          return data;
+        });
+      });
   }
 
   addNewItem() {
-    if (this.inputValue.content != "") {
+    if (this.inputValue.content != '') {
       this.inputValue.datemodified = new Date();
       this.inputValue.isDone = false;
       this.todoCollection.add(this.inputValue);
-      this.inputValue.content = "";
-      this.openSnackBar("Added Successfuly!", "Dismiss");
+      this.inputValue.content = '';
+      this.openSnackBar('Added Successfuly!', 'Dismiss');
     }
   }
 
   deleteItem(i) {
     this.todoDoc = this.afs.doc(`Todolist/${i}`);
     this.todoDoc.delete();
-    this.openSnackBar("Item Deleted!", "Dismiss");
+    this.openSnackBar('Item Deleted!', 'Dismiss');
   }
   editItem(i) {
+    this.openDialog();
     this.inputValue.content = i.content;
     this.editValue = true;
     this.inputId = i.id;
   }
   markItemAsDone(item) {
-    this.inputValue.content = item.content;
-    this.inputValue.isDone = true;
+    // this.inputValue.content = item.content;
+    // this.inputValue.isDone = true;
+    // this.todoDoc = this.afs.doc(`Todolist/${item.id}`);
+    // this.todoDoc.update(this.inputValue);
+    // this.inputValue.content = '';
     this.todoDoc = this.afs.doc(`Todolist/${item.id}`);
-    this.todoDoc.update(this.inputValue);
-    this.inputValue.content = "";
-    this.openSnackBar("Item Done!", "Dismiss");
+    this.todoDoc.delete();
+    this.openSnackBar('Item Done!', 'Dismiss');
   }
   markItemAsNotDone(item) {
     this.inputValue.content = item.content;
     this.inputValue.isDone = false;
     this.todoDoc = this.afs.doc(`Todolist/${item.id}`);
     this.todoDoc.update(this.inputValue);
-    this.inputValue.content = "";
-    this.openSnackBar("Item Not Done!", "Dismiss");
+    this.inputValue.content = '';
+    this.openSnackBar('Item Not Done!', 'Dismiss');
   }
   saveNewItem() {
-    if (this.inputValue.content != "") {
+    if (this.inputValue.content != '') {
       this.inputValue.isDone = false;
       this.inputValue.datemodified = new Date();
       this.todoDoc = this.afs.doc(`Todolist/${this.inputId}`);
       this.todoDoc.update(this.inputValue);
       this.editValue = false;
-      this.inputValue.content = "";
-      this.openSnackBar("Updated Successfuly!", "Dismiss");
+      this.inputValue.content = '';
+      this.openSnackBar('Updated Successfuly!', 'Dismiss');
     }
   }
   openSnackBar(message: string, action: string) {
@@ -96,7 +111,46 @@ export class TodoComponent implements OnInit {
       verticalPosition: 'top',
     });
   }
-  toggleCheck(i) {
+  toggleCheck(i) {}
+  openDialog(): void {
+    const dialogRef = this.dialog.open(TodoModal, {
+      width: '250px',
+      data: {
+        inputValue: this.inputValue,
+        // content: this.inputValue.content,
+        // priority: this.inputValue.priority,
+      },
+    });
 
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      this.inputValue.content = result;
+      if (this.editValue) {
+        console.log('inputValue: ', result);
+        this.saveNewItem();
+      } else {
+        console.log('inputValue: ', result);
+        this.addNewItem();
+      }
+    });
+  }
+}
+
+@Component({
+  selector: 'todo-modal',
+  templateUrl: './todo-modal.component.html',
+})
+export class TodoModal {
+  // todoCollection: AngularFirestoreCollection<Todo>;
+  constructor(
+    public snackBar: MatSnackBar,
+    public dialogRef: MatDialogRef<TodoModal>,
+    @Inject(MAT_DIALOG_DATA) public data: Todo
+  ) {
+    console.log('data: ', data);
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
